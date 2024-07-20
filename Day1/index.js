67628c5d360351ebdda1a5faf1ad5354c14e2011
Abhishek kumar <abhisheknextabhi@gmail.com>
2024-07-20 15:49:47 +0530

    Day1

---
+++ Day1/index.js
@@ -0,0 +1,34 @@
+// Activity 1 Variable Declartion
+ //Task 1
+ var number=10;
+ console.log(number)
+//Task 2
+let string="abhi"
+console.log(string)
+// Activity 2 Constant Declartion
+// Task 3
+const boolen=true;
+console.log(boolen)
+// Activity 3 Data Types
+// Task 4
+let arr=[1,2,3]
+let obj={
+    hello:"abhi"
+}
+console.log(typeof(number))
+console.log(typeof(string))
+console.log(typeof(boolen))
+console.log(typeof(arr))
+console.log(typeof(obj))
+// Activity Reassigning value
+// Task5
+let val=10
+console.log(val)
+val=20
+console.log(val)
+// Activity Understanding const and observe error
+// Task 6
+const value=20
+console.log(value)
+value=30
+console.log(value)
